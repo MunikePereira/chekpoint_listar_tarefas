6e91682d3c742844287eab9b04be31833b00a9b0
Munike Nicole Pereira Lima <munikepereira13@gmail.com>
2026-04-22 20:46:55 -0300

    adicionamos novas funco~es e finalizamos o projeto

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from tarefas import adicionar_tarefa, listar_tarefas
+from tarefas import adicionar_tarefa, listar_tarefas, concluir_tarefa, salvar_dados, carregar_dados
 import time
 
-TEMPO = 10
-TEMPO_TAREFA = 15
+TEMPO = 3
+TEMPO_TAREFA = 5
 TEMPO_ERRO = 5
-
+carregar_dados()
 while True:
     print("============= MENU ============")
     print("||       1. ADICIONAR        ||")
     print("||       2. LISTAR           ||")
-    print("||       3. SAIR             ||")
+    print("||       3. ATUALIZAR LISTA  ||")
+    print("||       4. SAIR             ||")
 
     opcoes = input("Digite uma opção: ")
 
@@ -17,12 +18,18 @@ while True:
         case "1":
             descricao_usuario = input("Adicione uma descrição: ")
             adicionar_tarefa(descricao_usuario)
+            salvar_dados()
         
         case "2":
             listar_tarefas()
             time.sleep(TEMPO_TAREFA)
-
+        
         case "3":
+            i = int(input("digite o indice que quer concluir: "))
+            concluir_tarefa(i)
+            salvar_dados()
+
+        case "4":
             print("Fechando programa...")
             time.sleep(TEMPO)
             break
@@ -31,6 +38,9 @@ while True:
             print("Opcão invalída")
             time.sleep(TEMPO_ERRO)
 
+
+ 
+    
 
  
     
--- tarefas.py
+++ tarefas.py
@@ -1,3 +1,4 @@
+import json
 lista_tarefas = []
 
 def adicionar_tarefa(descricao):
@@ -7,24 +8,53 @@ def adicionar_tarefa(descricao):
     }
 
     lista_tarefas.append(tarefa)
-    print("Tarefa adicionada com sucesso!")
 
-    return descricao 
+    salvar_dados()
+    print("Tarefa adicionada com sucesso!")
+     
 
 
 
 def listar_tarefas():
 
     print("=========== LISTAR TAREFAS ==========")
-    for tarefa in lista_tarefas:
+    for indice, tarefa in enumerate(lista_tarefas):
         if tarefa['concluida'] == True:     
-            print(f"{tarefa['descricao']} ✅")
+            print(f"{indice}. {tarefa['descricao']} ✅")
         else:
-            print(f"{tarefa['descricao']} ❌")
+            print(f"{indice}. {tarefa['descricao']} ❌")
 
 def concluir_tarefa(indice):
-    print("Deseja")
-        
-        
+   
+    try: 
+        tarefa = lista_tarefas[indice] 
+        if not tarefa['concluida']:
+            tarefa['concluida'] = True
+            print(f"{indice}. {tarefa['descricao']} ✅ ")
+        else: 
+            print("Está tarefa ja esta concluida")
+    except TypeError:
+        print("ERRO: Digite o número inteiro")
+    except IndexError:
+        print("ERRO: digite um número que esta na lista")
 
 
+def salvar_dados():
+    global lista_tarefas
+    with open(".\\checkpoint\chekpoint_listar_tarefas\dados.json", "w") as arquivo:
+        json.dump(lista_tarefas, arquivo)
+        print("Dados salvo com sucesso!")
+
+
+def carregar_dados():
+        global lista_tarefas
+        try:
+            with open(".\\checkpoint\\chekpoint_listar_tarefas\\dados.json", "r") as arquivo:
+               lista_tarefas = json.load(arquivo)
+               print("Dados carregando...😊")
+            
+        except FileNotFoundError:
+            print("Nenhum dado. \n Iniciando!...")
+            lista_tarefas = []
+        
+        
